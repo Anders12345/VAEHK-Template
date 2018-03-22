e8ca104be0de5dae684d0294b10f91905690706a
Anders12345 <andersskatan@hotmail.com>
2018-03-22 14:02:24 +0100

    Added 404.php

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found).
+ *
+ * 
+ */
+ ?>
+<?php get_header(); ?>
+ <div class="content">
+	<section class="main">
+		<p>Fant ikke siden du prøvde å åpne.</p>
+
+	</section> <!-- .main -->
+	<?php get_sidebar(); ?>
+</div><!-- .content -->
+
+<?php get_footer(); ?>
