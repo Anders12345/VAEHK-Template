c25d466a73125d2376dd26594f75f492601b4c03
Anders12345 <andersskatan@hotmail.com>
2018-03-22 15:14:19 +0100

    Changed blog-post div to article

--- content.php
+++ content.php
@@ -1,8 +1,8 @@
-<div class="blog-post">
+<article class="blog-post">
 	<div class="fimage"><?php the_post_thumbnail('vaehk_thumbnail'); ?></div>
 	<div class="post-info">
 		<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		
 		<?php //the_content(); ?>
 	</div>
-</div>
+</article>
